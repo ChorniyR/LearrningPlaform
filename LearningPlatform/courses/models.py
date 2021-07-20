97f64c583c047a764c3d21ba4fc540388266fff1
ChorniyR <pomak803@gmail.com>
2021-07-20 15:34:24 +0300

    create models

--- LearningPlatform/courses/models.py
+++ LearningPlatform/courses/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Course(models.Model):
+    name = models.CharField(max_length=64)
+    description = models.TextField()
+    details = models.TextField()
+    start_date = models.DateField()
+    end_date = models.DateField()
+    creating_date = models.DateTimeField(auto_now_add=True)
+    teacher = models.ForeignKey('Teacher', on_delete=models.SET_NULL, null=True)
+    
