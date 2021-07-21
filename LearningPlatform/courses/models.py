86d7e5e4102e7d66e2145e33c2512d3c5da1f068
ChorniyR <pomak803@gmail.com>
2021-07-21 11:10:02 +0300

    create models

--- LearningPlatform/courses/models.py
+++ LearningPlatform/courses/models.py
@@ -11,4 +11,4 @@ class Course(models.Model):
     end_date = models.DateField()
     creating_date = models.DateTimeField(auto_now_add=True)
     teacher = models.ForeignKey('Teacher', on_delete=models.SET_NULL, null=True)
-    
+
