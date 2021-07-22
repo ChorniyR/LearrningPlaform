5beb3b572712e1d7808724b5f53004bcc5fad299
ChorniyR <pomak803@gmail.com>
2021-07-22 16:53:40 +0300

    add validator in CoursesSerializer

--- LearningPlatform/courses/serializers.py
+++ LearningPlatform/courses/serializers.py
@@ -6,5 +6,9 @@ from .models import Course
 class CoursesSerializer(serializers.ModelSerializer):
     class Meta:
         model = Course
-        fields = ['name', 'description', 'details', 'start_date', 'end_date', 'creating_date',]
+        fields = ['name', 'description', 'details', 'start_date', 'end_date', 'creating_date', ]
 
+    def validate(self, attrs):
+        if attrs['start_date'] > attrs['end_date']:
+            raise serializers.ValidationError("Start date should be as early as end_date")
+        return True
