f2aef70ceb66ce95cb781737f538a180d2a8be78
ChorniyR <pomak803@gmail.com>
2021-07-22 16:53:40 +0300

    fix validation bug

--- LearningPlatform/courses/serializers.py
+++ LearningPlatform/courses/serializers.py
@@ -4,7 +4,13 @@ from .models import Course
 
 
 class CoursesSerializer(serializers.ModelSerializer):
+    author = serializers.HiddenField(default=serializers.CurrentUserDefault())
+
     class Meta:
         model = Course
-        fields = ['name', 'description', 'details', 'start_date', 'end_date', 'creating_date',]
+        fields = ['name', 'description', 'details', 'start_date', 'end_date', 'creating_date', 'author']
 
+    def validate(self, attrs):
+        if attrs['start_date'] > attrs['end_date']:
+            raise serializers.ValidationError("Start date should be as early as end_date")
+        return attrs
