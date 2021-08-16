227b46701308c53ee793c7edc472e9f3f8c5d68a
roman <pomak803@gmail.com>
2021-08-16 21:24:12 +0300

    create vueapp

---
+++ learnig_platform-project/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <Header/>
+  <router-view/>
+</template>
+
+<script>
+import { ref } from 'vue'
+import { Header } from './components/Header.vue'
+
+export default {
+  name: 'LayoutDefault',
+
+  components: {
+    Header
+  },
+
+  setup () {
+    return {
+      leftDrawerOpen: ref(false)
+    }
+  }
+}
+</script>
---
+++ learnig_platform-project/src/components/Header.vue
@@ -0,0 +1,21 @@
+<template>
+    <q-header reveal elevated>
+        <q-toolbar>
+            <q-btn flat round dense icon="menu" class="q-mr-sm" />
+            <q-avatar>
+                <img src="https://cdn.quasar.dev/logo-v2/svg/logo-mono-white.svg">
+            </q-avatar>
+
+            <q-toolbar-title>Quasar Framework</q-toolbar-title>
+
+            <q-btn flat round dense icon="whatshot" />
+        </q-toolbar>
+      </q-header>
+</template>
+
+
+<script>
+export default {
+    name: 'Header'
+}
+</script>
