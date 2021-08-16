d1476e9ea670357bbb474895be2a13713b406adc
roman <pomak803@gmail.com>
2021-08-16 22:04:12 +0300

    create Login view

--- learnig_platform-project/src/components/Header.vue
+++ learnig_platform-project/src/components/Header.vue
@@ -1,31 +1,30 @@
 <template>
   <q-layout view="hHh lpR fFf">
-
     <q-header bordered class="bg-primary text-white" height-hint="98">
-      <q-toolbar>
-        <q-toolbar-title>
-          <q-avatar>
+        <q-toolbar>
+            <q-toolbar-title>
+            <q-avatar>
             <img src="https://upload.wikimedia.org/wikipedia/commons/f/f7/Bananas.svg">
-          </q-avatar>
-          Courses platform
+            </q-avatar>
+            Courses platform
         </q-toolbar-title>
 
         <q-btn dense flat round icon="menu" @click="toggleRightDrawer" />
-      </q-toolbar>
+        </q-toolbar>
 
-      <q-tabs align="left">
-        <q-route-tab to="/page1" label="Home" />
-        <q-route-tab to="/page2" label="Courses store" />
-        <q-route-tab to="/page3" label="Profile" />
-      </q-tabs>
+        <q-tabs align="left">
+            <q-route-tab to="/page1" label="Home" />
+            <q-route-tab to="/page2" label="Store" />
+            <q-route-tab to="/page3" label="Profile" />
+        </q-tabs>
     </q-header>
 
     <q-drawer show-if-above v-model="rightDrawerOpen" side="right" bordered>
-      <!-- drawer content -->
+       
     </q-drawer>
 
     <q-page-container>
-      <router-view />
+        <router-view/>
     </q-page-container>
 
   </q-layout>
