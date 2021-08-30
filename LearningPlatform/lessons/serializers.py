e1f6fde72ba4349d71c7147cb1777fe6eed21538
roman <pomak803@gmail.com>
2021-08-30 15:58:54 +0300

    update lessons serializer

--- LearningPlatform/lessons/serializers.py
+++ LearningPlatform/lessons/serializers.py
@@ -4,6 +4,18 @@ from .models import Lesson, Step, StepUser
 from quizzes.serializers import TestSerializer
 from reading_materials.serializers import ReadingMaterialSerializer
 
+class StepSerializerReadOnly(serializers.ModelSerializer):
+    """
+    This serializer is used to viewing title and number of step.
+    """
+    class Meta:
+        model = Step
+        fields = ['number', 'title']
+        extra_kwargs = {
+            'number': {'read_only': True},
+            'title': {'read_only': True}
+        }
+
 
 class StepSerializer(serializers.ModelSerializer):
     test = TestSerializer(read_only=True)
@@ -18,6 +30,8 @@ class StepSerializer(serializers.ModelSerializer):
 
 
 class LessonSerializer(serializers.ModelSerializer):
+    steps = StepSerializerReadOnly(many=True, read_only=True)
+
     class Meta:
         model = Lesson
         fields = ['title', 'steps']
